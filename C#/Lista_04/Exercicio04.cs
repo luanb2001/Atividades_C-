3345813a664037bfb2b1ea0dd4bf39d34f79ac7d
Luan Barbosa <88410785+luanb2001@users.noreply.github.com>
2022-02-15 19:24:23 -0300

    Add files via upload

---
+++ C#/Lista_04/Exercicio04.cs
@@ -0,0 +1,14 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		 int[] arr = { 1, 2, 3, 4 };
+
+        foreach (int cont in arr) 
+		{
+           Console.WriteLine(cont);
+		}
+	}
+}
